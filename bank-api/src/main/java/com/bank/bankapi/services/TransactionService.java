8dae77543b6da36a0251a7e1107a9089fe62f9e5
avishagarwal <avishagarwal@gmail.com>
2021-11-18 21:03:07 +0530

    Testing Itext

--- bank-api/src/main/java/com/bank/bankapi/services/TransactionService.java
+++ bank-api/src/main/java/com/bank/bankapi/services/TransactionService.java
@@ -3,8 +3,11 @@ package com.bank.bankapi.services;
 import com.bank.bankapi.domain.Account;
 import com.bank.bankapi.domain.Transaction;
 import com.bank.bankapi.exceptions.BAuthException;
+import com.itextpdf.text.DocumentException;
+
+import java.io.FileNotFoundException;
 
 public interface TransactionService {
     Integer createTransaction(Account from, Account to, double amount)throws BAuthException;
-    boolean getTransaction(String start ,String stop, int accountNumber) throws BAuthException;
+    boolean getTransaction(String start ,String stop, int accountNumber) throws BAuthException, FileNotFoundException, DocumentException;
 }
--- bank-api/src/main/java/com/bank/bankapi/services/TransactionServiceImpl.java
+++ bank-api/src/main/java/com/bank/bankapi/services/TransactionServiceImpl.java
@@ -5,11 +5,21 @@ import com.bank.bankapi.domain.Transaction;
 import com.bank.bankapi.exceptions.BAuthException;
 import com.bank.bankapi.repositories.AccountRepository;
 import com.bank.bankapi.repositories.TransactionRepository;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -37,8 +47,31 @@ public class TransactionServiceImpl implements TransactionService{
     }
 
     @Override
-    public boolean getTransaction(String start, String stop, int accountNumber) throws BAuthException {
+    public boolean getTransaction(String start, String stop, int accountNumber) throws BAuthException, FileNotFoundException, DocumentException {
         List<Transaction> list= transactionRepository.getTransactions(start,stop,accountNumber);
+        Document document=new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("transactions.pdf"));
+        document.open();
+        PdfPTable table= new PdfPTable(3);
+        addTableHeader(table);
+        addRows(table);
+        document.add(table);
+        document.close();
         return true;
+    }
+    private void addTableHeader(PdfPTable table) {
+        Stream.of("column header 1", "column header 2", "column header 3")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    table.addCell(header);
+                });
+    }
+    private void addRows(PdfPTable table) {
+        table.addCell("row 1, col 1");
+        table.addCell("row 1, col 2");
+        table.addCell("row 1, col 3");
     }
 }

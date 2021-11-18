5724877bbc50517d9508e0f898be80249d488df7
avishagarwal <avishagarwal@gmail.com>
2021-11-18 16:51:29 +0530

    Added sql for transaction table

--- bank_db.sql
+++ bank_db.sql
@@ -52,7 +52,22 @@ last_updated_at timestamp with time zone DEFAULT now()
 alter table bt_accounts add constraint user_acc_fk
 foreign key (user_id) references bt_users(user_id);
 
+create table bt_transactions(
+transaction_id integer primary key not null,
+from_id integer not null,
+to_id integer not null,
+amount numeric(11,3) not null,
+from_balance numeric(11,3) not null,
+to_balance numeric(11,3) not null,
+created_at timestamp with time zone DEFAULT now()
+);
+alter table bt_transactions add constraint acc_trans_from_fk
+foreign key (from_id) references bt_accounts(account_number);
+alter table bt_transactions add constraint acc_trans_to_fk
+foreign key (to_id) references bt_accounts(account_number);
+
 create sequence bt_employees_seq increment 1 start 1;
 create sequence bt_users_seq increment 1 start 1;
 create sequence bt_accounts_seq increment 1 start 1;
+create sequence bt_transactions_seq increment 1 start 1;
 
